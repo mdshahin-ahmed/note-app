27bd2c655fa3b6b3ea08ccf326cb64dc91ada321
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-06-07 23:10:16 +0600

    Start project

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView>
+        <Text>Alhamdulillah</Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  main: {
+    backgroundColor: "orange",
+    color: "white",
+  },
+});
